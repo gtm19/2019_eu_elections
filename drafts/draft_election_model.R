f5aa018ddcf17d410c8fbe24f0fa3f8db260d8cd
gtm19 <9137502+gtm19@users.noreply.github.com>
2019-05-11 14:46:44 +0100

    Little more work on `elect` function.

--- drafts/draft_election_model.R
+++ drafts/draft_election_model.R
@@ -5,47 +5,34 @@ library(readxl)
 library(here)
 library(sf)
 
+source(here("R/allocate_dhondt.R"))
+
 # Candidate data
 candidates <- read_csv(here("data/2019_eu_candidates_uk.csv"))
 
-# Number of seats per region
-seats <- read_csv(here("data/2019_eu_seats_uk.csv")) %>% 
-  {set_names(.$seats, .$region)}
+# Polls data
+source(here("data/cleansing_scripts/polls.R"))
 
-election <- function(.candidate_df, .seats, n = 2) {
+
+
+elect <- function(candidate_df = candidates) {
   
-  nested <- .candidate_df %>% 
-    mutate(elected = FALSE) %>% 
-    group_by(region_id, region) %>% 
-    nest() %>% 
-    mutate(seats = .seats[region])
+  candidate_df <- candidate_df[!candidate_df$withdrawn,]
   
-  return(nested)
+  # Randomly allocate votes to ballot options
+  .votes <- sapply(split(candidate_df$on_the_ballot, candidate_df$region), function(parties){
+    unique_parties <- unique(parties)
+    
+    setNames(runif(length(unique_parties), 1000, 200000), unique_parties)
+  })
+  
+  .max_seats <- sapply(split(candidates$on_the_ballot, candidates$region), table)
+  
+  return({
+    list(.votes, .max_seats)
+  })
+  
 }
 
-test <-
-  election(candidates, seats)
+elect()
 
-
-
-test$data
-constituency <- function(.df, .region = "North West") {
-  tbl <-
-    .df %>% 
-    filter(region == .region)
-  
-  parties <-
-    tbl %>% 
-    pull(on_the_ballot) %>% 
-    unique() %>% 
-    sort()
-  
-  votes <- 
-    runif(length(parties), 0, 100) %>% 
-    {. / sum(.)} %>% 
-    set_names(parties) %>% 
-    sort(decreasing = TRUE)
-  
-  return(names(votes[1]))
-  
-}
